1299f5f4c7e6a42835e359be56ced4814df9d2d3
drummpy <154277179+drummpy@users.noreply.github.com>
2025-04-14 19:15:09 -0700

    Add Cell factory and edited board creation

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 function Gameboard() {
   //Create Game Board
-  const board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  const board = [];
+
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i].push(Cell());
+    }
+  }
 
   //Returns the board
   const getBoard = () => board;
 
   const placeMove = (row, column, player) => {
     //Check to see if cell is empty, if not return
-    if (board[row][column] !== "") {
+    if (board[row][column] !== 0) {
       return;
     }
 
@@ -21,8 +24,22 @@ function Gameboard() {
 
   //Prints board into console, joining each value with a |
   const printBoard = () => {
-    board.forEach((row) => console.log(row.join(" | ")));
+    board.forEach((row) =>
+      console.log(row.map((cell) => cell.getValue()).join(" | "))
+    );
   };
 
   return { getBoard, placeMove, printBoard };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return { addToken, getValue };
 }
